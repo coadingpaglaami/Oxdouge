4d29c722ba23b1f074bd91da93ac0648b0bbcc54
Mehedy Hasan  Piale <mehedyhasan4954@gmail.com>
2025-10-21 16:31:44 +0600

    feat: done the home except one section

--- src/appcomponent/landingpage/HeaterSolution.tsx
+++ src/appcomponent/landingpage/HeaterSolution.tsx
@@ -1,11 +1,12 @@
-'use client';
+"use client";
 import { landingPageData } from "@/data";
 import { Button } from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { Heater } from "../reusable";
 
 export const HeaterSolution = () => {
-    const router= useRouter();
+  const router = useRouter();
   return (
     <div className="flex flex-col">
       {/* ----------- Top Section ----------- */}
@@ -25,45 +26,21 @@ export const HeaterSolution = () => {
       {/* ----------- Card Section ----------- */}
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 ">
         {landingPageData.map((item, index) => (
-          <div
+          <Heater
+            img={item.img}
+            description={item.description}
+            price={item.price}
+            subtitle={item.subtitle}
+            title={item.title}
             key={index}
-            className="flex flex-col bg-[#121212] border border-primary rounded-lg overflow-hidden"
-          >
-            {/* Image */}
-            <img
-              src={item.img}
-              alt={item.title}
-              className="w-full h-56 object-cover"
-            />
-
-            {/* Content */}
-            <div className="flex flex-col p-6 gap-3">
-              {/* Subtitle */}
-              <p className="text-sm text-[#9C9C9C]">{item.subtitle}</p>
-
-              {/* Title */}
-              <h3 className="text-xl font-semibold text-white">
-                {item.title}
-              </h3>
-
-              {/* Description */}
-              <p className="text-gray-400 text-sm">{item.description}</p>
-
-              {/* Price */}
-              <p className="text-lg font-semibold text-white mt-2">
-                {item.price}
-              </p>
-
-              {/* Add to Cart Button */}
-              <Button className="mt-3 flex items-center justify-center gap-2">
-                <ShoppingCart className="w-4 h-4" />
-                Add to Cart
-              </Button>
-            </div>
-          </div>
+          />
         ))}
       </div>
-      <Button variant='outline' className="self-center mt-8 text-white" onClick={()=>router.push('/products')} >
+      <Button
+        variant="outline"
+        className="self-center mt-8 text-white"
+        onClick={() => router.push("/products")}
+      >
         View All Product
       </Button>
     </div>
--- src/appcomponent/reusable/Heater.tsx
+++ src/appcomponent/reusable/Heater.tsx
@@ -1,5 +1,18 @@
-export const Heater =() => {
-    return (
-        <div>Heater Component</div>
-    )
-}
+import { Button } from "@/components/ui/button";
+import { DiselHeater } from "@/interfaces";
+import { ShoppingCart } from "lucide-react";
+
+export const Heater = ({ img, title, subtitle, description, price }:DiselHeater) => (
+  <div className="flex flex-col bg-[#121212] border border-primary rounded-lg overflow-hidden">
+    <img src={img} alt={title} className="w-full h-56 object-cover" />
+    <div className="flex flex-col p-6 gap-3">
+      <p className="text-sm text-[#9C9C9C]">{subtitle}</p>
+      <h3 className="text-xl font-semibold text-white">{title}</h3>
+      <p className="text-gray-400 text-sm">{description}</p>
+      <p className="text-lg font-semibold text-white mt-2">{price}</p>
+      <Button className="mt-3 flex items-center justify-center gap-2">
+        <ShoppingCart className="w-4 h-4" /> Add to Cart
+      </Button>
+    </div>
+  </div>
+);
